507a925a403d077f555ac44c8d2031f2fd663eca
Tarun yadav <104274276+tarunvyadav@users.noreply.github.com>
2023-01-21 22:26:42 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,17 +12,14 @@ function Cart() {
   
    const cart= useSelector((state)=>state.rcart.cart)
    const [total, setTotal] = useState(0)
-   
-  
    const dispatch= useDispatch();
+  
+   
 
 
-  useEffect(()=>{
+   useEffect(()=>{
     setTotal(cart.reduce((acc, curr) => acc +curr.cart.price*curr.qyt,0));
    
-    console.log(cart)  
-      
-       
    },[cart])
 
  
@@ -30,53 +27,50 @@ function Cart() {
 
   return (
     <>
-     <div>{total}</div>
+    {!cart.length ? <div className='emptyCart'>Cart Is Empty!!!</div>: 
     <div className='cart'>
-     {cart.map((item)=>{
+       {cart.map((item)=>{
        const { title,image, price, category,}= item.cart;
-
-      const IncreseQyt=()=>{
-             
-        return dispatch(increaseQtyAction(item))
-      }
+       const IncreseQyt=()=>{
+              return dispatch(increaseQtyAction(item))
+        }
          
-      const decreseQyt=()=>{
-             
-        return dispatch(decreaseQtyAction(item))
-      }
+       const decreseQyt=()=>{
+            return dispatch(decreaseQtyAction(item))
+       }
 
-      const remove= async()=>{
-   
-        return dispatch(RemoveFromCart(item))
-      }
+       const remove= async()=>{
+             return dispatch(RemoveFromCart(item))
+       }
 
        return(
-        <><div key={item.id} className='cartCard'>
-          <div className='imgDiv'><img src={image}/>
-          </div>
-           <div className='textDiv' >
-            <h2>{title}</h2>
-            <p>{category}</p>
-            <div className='money'>
+        <>
+          <div key={item.id} className='cartCard'>
+            <div className='imgDiv'><img src={image}/></div>
+            <div className='textDiv' >
+               <h2>{title}</h2>
+               <p>{category}</p>
+               <div className='money'>
             
-            <div className='qyt'><div className='qytText'>{item.qyt}</div>
-             <div className='arrow'>
-                <ArrowDropUpIcon className='ArrowDropUpIcon' onClick={IncreseQyt} /> 
-                <ArrowDropDownIcon className='ArrowDropDownIcon' onClick={decreseQyt} />
+                 <div className='qyt'><div className='qytText'>{item.qyt}</div>
+                   <div className='arrow'>
+                    <ArrowDropUpIcon className='ArrowDropUpIcon' onClick={IncreseQyt} /> 
+                    <ArrowDropDownIcon className='ArrowDropDownIcon' onClick={decreseQyt} />
+                   </div>
+                 </div>
+                 <h4>${price}</h4>
+                 <DeleteIcon onClick={remove}/>
+               </div>
             </div>
-             </div>
-            <h4>${price}</h4>
-            <DeleteIcon onClick={remove}/>
-            </div>
-          </div>
           </div>
         </>
        )
      })}
-        <div className='total'><div className='text'>Total Numbers of Items</div>
-                        <div className='totalNo'>${total}</div>
-                        </div>
-    </div>
+        <div className='total'>
+          <div className='text'>Total Numbers of Items</div>
+          <div className='totalNo'>${total}</div>
+        </div>
+    </div>}
  </>
   )
 }
